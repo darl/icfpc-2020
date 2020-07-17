1e78664593ecfac7aed4e1c651c1fa37dafc2c19
Vladislav Dolbilov <darl@yandex-team.ru>
2020-07-17 21:41:16 +0300

    simplify

--- src/main/scala/icfpc/classified/Expression.scala
+++ src/main/scala/icfpc/classified/Expression.scala
@@ -8,24 +8,22 @@ case class UnknownVariable(value: Int) extends Expression
 case class Apply(op: Expression, arg: Expression) extends Expression
 
 case object Nil extends Expression
+
 case class Cons(head: Expression, tail: Expression) extends Expression
 
 case object False extends Expression
 case object True extends Expression
 
-case object Sum0 extends Expression
-case class Sum1(left: Expression) extends Expression // = Apply(Sum0, left)
+case class Sum(left: Expression, right: Expression) extends Expression
 
-case object Product0 extends Expression
-case class Product1(left: Expression) extends Expression
+case class Product(left: Expression, right: Expression) extends Expression
 
-case object Negate extends Expression
+case class Negate(exp: Expression) extends Expression
 
-case object Div0 extends Expression
-case class Div1(left: Expression) extends Expression
+case class Div(left: Expression, right: Expression) extends Expression
 
 //successor
-case object Succ extends Expression
-case object Pred extends Expression
+case class Succ(exp: Expression) extends Expression
+case class Pred(exp: Expression) extends Expression
 
 case class Function(id: Int, expr: Expression) extends Expression
