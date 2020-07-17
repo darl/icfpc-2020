36e27a7b57a758d09b618c737fd8f0dad07dd1bf
Vladislav Dolbilov <darl@yandex-team.ru>
2020-07-17 22:31:00 +0300

    wip

--- src/main/scala/icfpc/classified/Expression.scala
+++ src/main/scala/icfpc/classified/Expression.scala
@@ -1,40 +1,39 @@
 package icfpc.classified
 
 sealed trait Expression
+sealed trait Basic extends Expression
 
-sealed trait Op extends Expression
-case class Apply(op: Expression, arg: Expression) extends Expression
-case class Literal(value: Int) extends Expression
+sealed trait Op extends Expression with Basic
+case class Apply(op: Expression, arg: Expression) extends Expression with Basic
 
-case class UnknownVariable(value: Int) extends Op
+case class Literal(value: Int) extends Expression with Basic
+case class UnknownVariable(value: Int) extends Op with Basic
 
-case object Nil extends Op
+case object Nil extends Expression with Basic with Op
 
-case object Cons0 extends Op
-case class Cons1(head: Expression) extends Op
-case class Cons(head: Expression, tail: Expression) extends Op
+case object Cons0 extends Expression with Basic with Op
+case class Cons1(head: Expression) extends Expression with Op
+case class Cons(head: Expression, tail: Expression) extends Expression with Op
 
-case object False extends Op
-case object True extends Op
+case object False extends Expression with Basic with Op
+case object True extends Expression with Basic with Op
 
-case object Sum0 extends Op
-case class Sum1(left: Literal) extends Op
+case object Sum0 extends Expression with Basic with Op
+case class Sum1(left: Expression) extends Expression with Op
 
-case object Product0 extends Op
-case class Product1(left: Expression) extends Op
-case class Product(left: Expression, right: Expression) extends Op
+case object Product0 extends Expression with Basic with Op
+case class Product1(left: Expression) extends Expression with Op
 
-case object Negate0 extends Op
-case class Negate(exp: Expression) extends Op
+case object Negate0 extends Expression with Basic with Op
 
-case object Div0 extends Op
-case class Div1(left: Expression) extends Op
-case class Div(left: Expression, right: Expression) extends Op
+case object Mul0 extends Expression with Basic with Op
+case class Mul1(left: Expression) extends Expression with Op
 
-//successor
-case object Succ0 extends Op
-case class Succ(exp: Expression) extends Op
-case object Pred0 extends Op
-case class Pred(exp: Expression) extends Op
+case object Div0 extends Expression with Basic with Op
+case class Div1(left: Expression) extends Expression with Op
+
+//increment
+case object Inc0 extends Expression with Basic with Op
+case object Dec0 extends Expression with Basic with Op
 
 case class Function(id: Int, expr: Expression) extends Op
--- src/main/scala/icfpc/classified/Parser.scala
+++ src/main/scala/icfpc/classified/Parser.scala
@@ -1,20 +1,58 @@
 package icfpc.classified
 
 class Parser {
-  def parseBinary(data: Iterator[Int]): Expression = ???
 
-  def parseText(data: Iterator[String]): Expression = ???
+  val basic: Seq[(String, Basic, Int)] = List(
+    ("inc", Inc0, 0),
+    ("dec", Dec0, 0),
+    ("mul", Mul0, 0),
+    ("div", Div0, 0),
+    ("nil", Nil, 0),
+    ("cons", Cons0, 0)
+  )
+
+  def parseBinary(data: List[Int]): Expression = ???
+
+  def parseText(data: List[String]): (Expression with Basic, List[String]) = {
+    data.head match {
+      case "ap" =>
+        val (op, rest) = parseText(data.tail)
+        val (arg, tail) = parseText(rest)
+        Apply(op, arg) -> tail
+      case v =>
+        basic.find(_._1 == v) match {
+          case Some(value) =>
+            value._2 -> data.tail
+          case None =>
+            if (v.startsWith(":")) UnknownVariable(v.stripPrefix(":").toInt) -> data.tail
+            else Literal(v.toInt) -> data.tail
+        }
+    }
+  }
 
   def printText(expression: Expression): String = {
-    expression match {
+    toBasic(expression) match {
       case Literal(value) => value.toString
       case UnknownVariable(value) => s":$value"
       case Apply(op, arg) => s"ap ${printText(op)} ${printText(arg)}"
-      case Nil => "nil"
-      case Cons0 => "cons"
-      case Cons1(tail) => printText(Apply(Cons0, tail))
-      case Cons(head, tail) => printText(Apply(Cons1(head), tail))
-      case Div(left, right) => s"ap ap div ${printText(left)} ${printText(right)}"
+      case v =>
+        basic.find(_._2 == v) match {
+          case Some(value) => value._1
+          case None => s"unknown($v)"
+        }
+    }
+  }
+
+  def toBasic(expression: Expression): Basic = {
+    expression match {
+      case b: Basic => b
+      case Cons1(head) => Apply(Cons0, head)
+      case Cons(head, tail) => Apply(Cons1(head), tail)
+      case Sum1(left) => Apply(Sum0, left)
+      case Product1(left) => Apply(Product0, left)
+      case Mul1(left) => Apply(Mul0, left)
+      case Div1(left) => Apply(Div0, left)
+      case Function(id, expr) => ???
     }
   }
 }
