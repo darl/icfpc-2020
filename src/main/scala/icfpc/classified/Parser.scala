dd2f846e1fa8909b19715bd3f62673e04bc74fdd
Vladislav Dolbilov <darl@yandex-team.ru>
2020-07-17 21:58:47 +0300

    return carried versions

--- src/main/scala/icfpc/classified/Parser.scala
+++ src/main/scala/icfpc/classified/Parser.scala
@@ -5,5 +5,16 @@ class Parser {
 
   def parseText(data: Iterator[String]): Expression = ???
 
-  def printText(expression: Expression): String = ???
+  def printText(expression: Expression): String = {
+    expression match {
+      case Literal(value) => value.toString
+      case UnknownVariable(value) => s":$value"
+      case Apply(op, arg) => s"ap ${printText(op)} ${printText(arg)}"
+      case Nil => "nil"
+      case Cons0 => "cons"
+      case Cons1(tail) => printText(Apply(Cons0, tail))
+      case Cons(head, tail) => printText(Apply(Cons1(head), tail))
+      case Div(left, right) => s"ap ap div ${printText(left)} ${printText(right)}"
+    }
+  }
 }
