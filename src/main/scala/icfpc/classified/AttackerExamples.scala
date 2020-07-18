568c227112bdeff2027b1b6de22a38b612d68300
mcsim <mcsim1993@gmail.com>
2020-07-19 02:39:06 +0300

    examples

---
+++ src/main/scala/icfpc/classified/AttackerExamples.scala
@@ -0,0 +1,91 @@
+package icfpc.classified
+
+object AttackerExamples {
+  List(
+    Literal(1),
+    Literal(1),
+    List(
+      Literal(256),
+      Literal(0),
+      List(Literal(512), Literal(1), Literal(64)),
+      List(Literal(16), Literal(128)),
+      List(Literal(1), Literal(2), Literal(3), Literal(4))
+    ),
+    List(
+      Literal(0),
+      List(Literal(16), Literal(128)),
+      List(
+        List(
+          List(
+            Literal(1),
+            Literal(0),
+            List(Literal(-48), Literal(36)),
+            List(Literal(0), Literal(0)),
+            List(Literal(1), Literal(2), Literal(3), Literal(4)),
+            Literal(0),
+            Literal(64),
+            Literal(1)
+          ),
+          Nil
+        ),
+        List(
+          List(
+            Literal(0),
+            Literal(1),
+            List(Literal(48), Literal(-36)),
+            List(Literal(0), Literal(0)),
+            List(Literal(10), Literal(10), Literal(10), Literal(10)),
+            Literal(0),
+            Literal(64),
+            Literal(1)
+          ),
+          Nil
+        )
+      )
+    )
+  )
+
+  List(
+    Literal(1),
+    Literal(1),
+    List(
+      Literal(256),
+      Literal(0),
+      List(Literal(512), Literal(1), Literal(64)),
+      List(Literal(16), Literal(128)),
+      List(Literal(112), Literal(64), Literal(4), Literal(16))
+    ),
+    List(
+      Literal(0),
+      List(Literal(16), Literal(128)),
+      List(
+        List(
+          List(
+            Literal(1),
+            Literal(0),
+            List(Literal(2), Literal(-48)),
+            List(Literal(0), Literal(0)),
+            List(Literal(112), Literal(64), Literal(4), Literal(16)),
+            Literal(0),
+            Literal(64),
+            Literal(1)
+          ),
+          Nil
+        ),
+        List(
+          List(
+            Literal(0),
+            Literal(1),
+            List(Literal(-2), Literal(48)),
+            List(Literal(0), Literal(0)),
+            List(Literal(10), Literal(10), Literal(10), Literal(10)),
+            Literal(0),
+            Literal(64),
+            Literal(1)
+          ),
+          Nil
+        )
+      )
+    )
+  )
+}
---
+++ src/main/scala/icfpc/classified/DefenderExamples.scala
@@ -0,0 +1,79 @@
+package icfpc.classified
+
+object DefenderExamples {
+  List(
+    Literal(1),
+    Literal(1),
+    List(Literal(256), Literal(1), List(Literal(448), Literal(1), Literal(64)), List(Literal(16), Literal(128)), Nil),
+    List(
+      Literal(0),
+      List(Literal(16), Literal(128)),
+      List(
+        List(
+          List(
+            Literal(1),
+            Literal(0),
+            List(Literal(48), Literal(31)),
+            List(Literal(0), Literal(0)),
+            List(Literal(10), Literal(10), Literal(10), Literal(10)),
+            Literal(0),
+            Literal(64),
+            Literal(1)
+          ),
+          Nil
+        ),
+        List(
+          List(
+            Literal(0),
+            Literal(1),
+            List(Literal(-48), Literal(-31)),
+            List(Literal(0), Literal(0)),
+            List(Literal(1), Literal(2), Literal(3), Literal(4)),
+            Literal(0),
+            Literal(64),
+            Literal(1)
+          ),
+          Nil
+        )
+      )
+    )
+  )
+
+  List(
+    Literal(1),
+    Literal(1),
+    List(Literal(256), Literal(1), List(Literal(448), Literal(1), Literal(64)), List(Literal(16), Literal(128)), Nil),
+    List(
+      Literal(0),
+      List(Literal(16), Literal(128)),
+      List(
+        List(
+          List(
+            Literal(1),
+            Literal(0),
+            List(Literal(-24), Literal(-48)),
+            List(Literal(0), Literal(0)),
+            List(Literal(10), Literal(10), Literal(10), Literal(10)),
+            Literal(0),
+            Literal(64),
+            Literal(1)
+          ),
+          Nil
+        ),
+        List(
+          List(
+            Literal(0),
+            Literal(1),
+            List(Literal(24), Literal(48)),
+            List(Literal(0), Literal(0)),
+            List(Literal(112), Literal(64), Literal(4), Literal(16)),
+            Literal(0),
+            Literal(64),
+            Literal(1)
+          ),
+          Nil
+        )
+      )
+    )
+  )
+}
