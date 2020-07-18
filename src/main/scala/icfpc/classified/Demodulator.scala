46a670f0a29e77fdb65aa4b23888f482a55b14f1
Harand97 <madnix7997@gmail.com>
2020-07-18 03:39:57 +0300

    modulation

---
+++ src/main/scala/icfpc/classified/Demodulator.scala
@@ -0,0 +1,70 @@
+package icfpc.classified
+
+object Demodulator {
+
+  def demodulate(binary: String): Expression = {
+    val result = demodulateNextChunk(binary)
+    if (result.tail.nonEmpty) throw new IllegalArgumentException("Illegal binary string")
+    result.chunk
+  }
+
+  private def demodulateNextChunk(binary: String): DemodulatedChunk =
+    if (isLiteralNext(binary)) demodulateLiteral(binary)
+    else demodulateList(binary)
+
+  private def demodulateList(binary: String): DemodulatedChunk = {
+    if (binary.startsWith("00")) {
+      DemodulatedChunk(Nil, binary.substring(2))
+    } else {
+      val withListData = binary.substring(2)
+      val head = demodulateNextChunk(withListData)
+      val tail = demodulateNextChunk(head.tail)
+      DemodulatedChunk(Cons(head.chunk, tail.chunk), tail.tail)
+    }
+  }
+
+  private def demodulateLiteral(binary: String): DemodulatedChunk = {
+    val (signData, withoutSign) = binary.splitAt(2)
+    val sign = signData match {
+      case "01" => 1
+      case "10" => -1
+      case _ => throw new IllegalArgumentException("Illegal literal start")
+    }
+
+    val (encodedWidth, withoutWidth) = withoutSign.splitAt(withoutSign.indexOf("0"))
+    val valueLength = encodedWidth.length * 4 + 1
+    val (encodedValue, tail) = withoutWidth.splitAt(valueLength)
+    DemodulatedChunk(Literal(sign * convertToDecimal(encodedValue)), tail)
+  }
+
+  private def convertToDecimal(binary: String): Int = {
+    val significantPartStart = binary.indexOf("1")
+    if (significantPartStart < 0) {
+      0
+    } else {
+      val significantPart = binary.substring(significantPartStart)
+      var value = 0
+      var power = significantPart.length - 1
+      for (s <- significantPart) {
+        val digit = s.toString.toInt
+        if (digit != 0 && digit != 1) throw new IllegalArgumentException(s"Illegal digit $digit")
+        value = value + digit * math.pow(2, power).toInt
+        power = power - 1
+      }
+      value
+    }
+  }
+
+  private def isLiteralNext(binary: String): Boolean =
+    binary.startsWith("01") || binary.startsWith("10")
+
+  case class DemodulatedChunk(chunk: Expression, tail: String)
+
+  class DemodulationState(var tail: String, var expr: Option[Expression])
+
+  sealed trait StackExpr
+
+  case class BasicExpr(expr: Expression) extends StackExpr
+  case class Expr(expr: Expression => Expression) extends StackExpr
+  case class HardExpr(expr: (Expression, Expression) => Expression) extends StackExpr
+}
---
+++ src/main/scala/icfpc/classified/Modulator.scala
@@ -0,0 +1,53 @@
+package icfpc.classified
+
+import scala.annotation.tailrec
+import scala.collection.immutable.{List => ScalaList}
+import scala.collection.mutable
+
+object Modulator {
+
+  def modulate(expression: Expression): String = {
+    val stack = mutable.Stack(expression)
+    val builder = new mutable.StringBuilder()
+    while (stack.nonEmpty) {
+      stack.pop match {
+        case Nil => builder.append("00")
+        case Literal(value) => builder.append(modulateLiteral(value))
+        case Cons(head, tail) =>
+          builder.append("11")
+          stack.push(tail)
+          stack.push(head)
+        case _ => throw new IllegalArgumentException("Can modulate only literals and lists of lists and literals")
+      }
+    }
+    builder.toString()
+  }
+
+  private def modulateLiteral(value: Int): String = {
+    val sign = if (value >= 0) "01" else "10"
+
+    val binaryValue = convertToBinary(math.abs(value), ScalaList.empty)
+    val addZeros =
+      if (binaryValue.length % 4 == 0) 0
+      else 4 - binaryValue.length % 4
+    val encodedValue =
+      if (value == 0) ""
+      else 1.to(addZeros).map(_ => 0).mkString + binaryValue
+    val encodedWidth =
+      if (value == 0) ""
+      else 1.to(encodedValue.length / 4).map(_ => 1).mkString
+
+    sign + encodedWidth + "0" + encodedValue
+  }
+
+  @tailrec
+  private def convertToBinary(curVal: Int, bin: ScalaList[Int]): String = {
+    if (curVal / 2 == 0) {
+      (curVal :: bin).mkString
+    } else {
+      val remains = curVal % 2
+      val divided = curVal / 2
+      convertToBinary(divided, remains :: bin)
+    }
+  }
+}
