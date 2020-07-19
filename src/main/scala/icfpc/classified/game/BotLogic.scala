a5ff2e256e4209f79d7bc78d91382db877b06041
Vladislav Dolbilov <darl@yandex-team.ru>
2020-07-19 13:45:22 +0300

    do not enable drive if not needed

--- src/main/scala/icfpc/classified/game/BotLogic.scala
+++ src/main/scala/icfpc/classified/game/BotLogic.scala
@@ -5,11 +5,13 @@ class BotLogic {
   def run(state: WorldState): Actions = {
     val targetSpeed = state.me.position.normal.widthLength(7)
     val targetForce = targetSpeed - state.me.speed
-    val move = Actions.moveDirection(targetForce)
+    val move =
+      if (state.me.heat < 48 && targetForce.length > 2) Actions.moveDirection(targetForce)
+      else Actions.empty
 
     val fire =
-      if (state.me.heat > 48) Actions.empty
-      else Actions.fire(state.enemy.position + state.enemy.speed)
+      if (state.me.heat <= 32) Actions.fire(state.enemy.position + state.enemy.speed)
+      else Actions.empty
 
     move |+| fire
   }
