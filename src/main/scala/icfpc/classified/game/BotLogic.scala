49249d652523b0929541c515351f00e46017735a
Harand97 <madnix7997@gmail.com>
2020-07-19 16:34:41 +0300

    logic funcs

--- src/main/scala/icfpc/classified/game/BotLogic.scala
+++ src/main/scala/icfpc/classified/game/BotLogic.scala
@@ -5,18 +5,21 @@ class BotLogic {
   def run(state: WorldState): Actions = {
     val targetSpeed = state.me.position.normal.widthLength(7)
     val targetForce = targetSpeed - state.me.speed
-    val move =
-      if (state.me.heat < 48 && targetForce.length > 2) Actions.moveDirection(targetForce)
-      else Actions.empty
+    val move = moveSafely(targetForce, state.me.heat)
 
-    val fire =
-      if (state.me.heat <= 32) {
-        val g = state.enemy.position.normalize.!
-        val fireDirection = state.enemy.position + state.enemy.speed + g
-        Actions.fire(fireDirection.round)
-      } else Actions.empty
+    val fire = fireDefault(state)
 
     move |+| fire
   }
 
+  def moveSafely(targetForce: Vector, curHeat: Int): Actions =
+    if (curHeat < 48 && targetForce.length > 2) Actions.moveDirection(targetForce)
+    else Actions.empty
+
+  def fireDefault(state: WorldState): Actions =
+    if (state.me.heat <= 32) {
+      val g = state.enemy.position.normalize.!
+      val fireDirection = state.enemy.position + state.enemy.speed + g
+      Actions.fire(fireDirection.round)
+    } else Actions.empty
 }
