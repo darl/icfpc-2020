ed8eb94f40270ce774f75a7dedf152a86b3f4e64
mcsim <mcsim1993@gmail.com>
2020-07-19 13:51:22 +0300

    better replay render

--- src/main/scala/icfpc/classified/replay/ReplayReader.scala
+++ src/main/scala/icfpc/classified/replay/ReplayReader.scala
@@ -1,8 +1,11 @@
 package icfpc.classified.replay
 
+import icfpc.classified.sandbox.StateAnnotator
+
 import scala.io.Source
 
 object ReplayReader extends App {
   val replays = Source.fromResource("rep_defender1.txt").getLines().toSeq
-//  ReplayPlayer(ReplayParser.parseLog(replays)).show()
+  val (worlds, states) = ReplayParser.parseLog(replays)
+  ReplayPlayer(worlds, states = states.map(s => StateAnnotator.annotate(s))).show()
 }
