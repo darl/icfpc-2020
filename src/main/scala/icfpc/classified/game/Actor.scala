c8020674c90dfc1ff2a444622676f9e738874b55
Vladislav Dolbilov <darl@yandex-team.ru>
2020-07-19 13:34:04 +0300

    keep heat low

--- src/main/scala/icfpc/classified/game/Actor.scala
+++ src/main/scala/icfpc/classified/game/Actor.scala
@@ -1,16 +1,22 @@
 package icfpc.classified.game
 
+import icfpc.classified.game.Actor.Stats
 import icfpc.classified.syntax._
 
-case class Actor(position: Vector, speed: Vector, supply: Int, health: Int)
+case class Actor(health: Int, position: Vector, speed: Vector, stats: Stats, heat: Int, x6: Int, x7: Int)
 
 object Actor {
 
+  case class Stats(supply: Int, x: Int, y: Int, z: Int)
+
   def from(exception: Expression): Actor = {
     val actor = exception.toList
-    val pos = actor.head.toList(2).toPair
-    val speed = actor.head.toList(3).toPair
+    val actorState = actor.head.toList
+    val pos = actorState(2).toPair
+    val speed = actorState(3).toPair
+    val stats = actorState(4).toList
     Actor(
+      health = 100,
       position = Vector(
         x = pos._1.toLiteral.value.toInt,
         y = pos._2.toLiteral.value.toInt
@@ -19,8 +25,15 @@ object Actor {
         speed._1.toLiteral.value.toInt,
         speed._2.toLiteral.value.toInt
       ),
-      supply = actor.head.toList(4).toList.head.toLiteral.value.toInt,
-      health = 100
+      stats = Stats(
+        stats(0).toLiteral.value.toInt,
+        stats(1).toLiteral.value.toInt,
+        stats(2).toLiteral.value.toInt,
+        stats(3).toLiteral.value.toInt
+      ),
+      heat = actorState(5).toLiteral.value.toInt,
+      x6 = actorState(6).toLiteral.value.toInt,
+      x7 = actorState(7).toLiteral.value.toInt
     )
   }
 }
--- src/main/scala/icfpc/classified/game/BotLogic.scala
+++ src/main/scala/icfpc/classified/game/BotLogic.scala
@@ -5,8 +5,13 @@ class BotLogic {
   def run(state: WorldState): Actions = {
     val targetSpeed = state.me.position.normal.widthLength(7)
     val targetForce = targetSpeed - state.me.speed
-    Actions.moveDirection(targetForce) |+|
-      Actions.fire(state.enemy.position + state.enemy.speed)
+    val move = Actions.moveDirection(targetForce)
+
+    val fire =
+      if (state.me.heat > 48) Actions.empty
+      else Actions.fire(state.enemy.position + state.enemy.speed)
+
+    move |+| fire
   }
 
 }
